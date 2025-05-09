5003e58d012ad130bf303021d743ee0c5e7c6e8d
Dr_Meg <megmontanez2000@yahoo.com>
2025-05-09 18:31:05 -0400

    Update visit_occurrence.sql
    Create visit_occurrence.sql

--- sql/visit_occurrence.sql
+++ sql/visit_occurrence.sql
@@ -1,10 +1,8 @@
--- visit_occurrence.sql
--- Transform MIMIC-III admissions data into OMOP VISIT_OCCURRENCE table
-
 SELECT
-  hadm_id AS visit_occurrence_id,
   subject_id AS person_id,
-  'Inpatient Visit' AS visit_source_value,
+  hadm_id AS visit_occurrence_id,
   admittime AS visit_start_datetime,
-  dischtime AS visit_end_datetime
-FROM mimic3_demo.admissions;
+  dischtime AS visit_end_datetime,
+  admission_type AS visit_source_value
+FROM `valued-aleph-459311-m0.mimic_demo.admissions`
+WHERE hadm_id IS NOT NULL;
