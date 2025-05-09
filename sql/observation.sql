0fd02aeb905ac6850a9bf3cb197e8a5104ad7d6c
Dr_Meg <megmontanez2000@yahoo.com>
2025-05-09 18:29:38 -0400

    Update observation.sql

--- sql/observation.sql
+++ sql/observation.sql
@@ -1,13 +1,8 @@
--- observation.sql
--- Transform selected MIMIC-III events into OMOP OBSERVATION table
-
 SELECT
-  l.row_id AS observation_id,
-  l.subject_id AS person_id,
-  l.hadm_id AS visit_occurrence_id,
-  d.label AS observation_source_value,
-  l.charttime AS observation_datetime,
-  l.value AS value_as_string
-FROM mimic3_demo.labevents l
-JOIN mimic3_demo.d_labitems d ON l.itemid = d.itemid
-WHERE l.valuenum IS NULL AND l.value IS NOT NULL;
+  subject_id AS person_id,
+  chartdate AS observation_date,
+  category AS observation_type,
+  description AS observation_source_value
+FROM `valued-aleph-459311-m0.mimic_demo.noteevents`
+WHERE description IS NOT NULL
+LIMIT 1000;
