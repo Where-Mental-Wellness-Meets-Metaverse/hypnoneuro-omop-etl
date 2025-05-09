c22f63023df58f43d2bb182bf2b4bafd78a729af
Dr_Meg <megmontanez2000@yahoo.com>
2025-05-09 18:27:31 -0400

    Update measurement.sql
    Update measurement.sql

--- sql/measurement.sql
+++ sql/measurement.sql
@@ -1,13 +1,9 @@
--- measurement.sql
--- Transform MIMIC-III lab events into OMOP MEASUREMENT table
-
 SELECT
-  l.row_id AS measurement_id,
-  l.subject_id AS person_id,
-  l.hadm_id AS visit_occurrence_id,
-  d.itemid AS measurement_source_value,
-  l.charttime AS measurement_datetime,
-  l.valuenum AS value_as_number,
-  l.valueuom AS unit_source_value
-FROM mimic3_demo.labevents l
-JOIN mimic3_demo.d_labitems d ON l.itemid = d.itemid;
+  subject_id AS person_id,
+  charttime AS measurement_datetime,
+  itemid AS measurement_source_value,
+  valuenum AS value_as_number,
+  valueuom AS unit_source_value
+FROM `valued-aleph-459311-m0.mimic_demo.labevents`
+WHERE valuenum IS NOT NULL
+LIMIT 1000;
