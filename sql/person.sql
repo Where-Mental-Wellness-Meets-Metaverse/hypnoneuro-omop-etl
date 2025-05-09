ca8906b17e448d2291fff49a1492c27f18538989
Dr_Meg <megmontanez2000@yahoo.com>
2025-05-09 18:19:01 -0400

    Update person.sql
    Update person.sql

--- sql/person.sql
+++ sql/person.sql
@@ -1,15 +1,12 @@
--- person.sql
--- Transform MIMIC-III patient data into OMOP PERSON table
-
-SELECT
-  subject_id AS person_id,
-  CASE
-    WHEN gender = 'M' THEN 'M'
-    WHEN gender = 'F' THEN 'F'
-    ELSE 'Unknown'
-  END AS gender_source_value,
-  EXTRACT(YEAR FROM dob) AS year_of_birth,
-  EXTRACT(MONTH FROM dob) AS month_of_birth,
-  EXTRACT(DAY FROM dob) AS day_of_birth,
-  ethnicity AS race_source_value
-FROM mimic3_demo.patients;
+SELECT  
+  subject_id AS person_id,  
+  CASE  
+    WHEN gender = 'M' THEN 'M'  
+    WHEN gender = 'F' THEN 'F'  
+    ELSE 'Unknown'  
+  END AS gender_source_value,  
+  EXTRACT(YEAR FROM dob) AS year_of_birth,  
+  EXTRACT(MONTH FROM dob) AS month_of_birth,  
+  EXTRACT(DAY FROM dob) AS day_of_birth,  
+  NULL AS race_source_value  
+FROM `valued-aleph-459311-m0.mimic_demo.patients`;
