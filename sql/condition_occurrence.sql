f26fd7a577473c4f0664c73dfead9e398ab90369
Dr_Meg <megmontanez2000@yahoo.com>
2025-05-09 18:25:43 -0400

    Update condition_occurrence.sql
    update condition_occurrence.sql

--- sql/condition_occurrence.sql
+++ sql/condition_occurrence.sql
@@ -1,26 +1,24 @@
--- condition_occurrence.sql
--- Transform MIMIC-III data into OMOP CONDITION_OCCURRENCE table
-
 WITH person_map AS (
-    SELECT DISTINCT subject_id AS person_id
-    FROM mimic3_demo.patients
+  SELECT DISTINCT subject_id AS person_id
+  FROM `valued-aleph-459311-m0.mimic_demo.patients`
 ),
 visit_map AS (
-    SELECT DISTINCT hadm_id AS visit_occurrence_id
-    FROM mimic3_demo.admissions
+  SELECT DISTINCT hadm_id AS visit_occurrence_id
+  FROM `valued-aleph-459311-m0.mimic_demo.admissions`
 ),
 condition_source AS (
-    SELECT
-        d.subject_id AS person_id,
-        d.hadm_id AS visit_occurrence_id,
-        d.icd9_code AS condition_source_value
-    FROM mimic3_demo.diagnoses_icd d
+  SELECT
+    d.subject_id AS person_id,
+    d.hadm_id AS visit_occurrence_id,
+    d.icd9_code AS condition_source_value
+  FROM `valued-aleph-459311-m0.mimic_demo.diagnoses_icd` d
 )
 
 SELECT
-    cs.person_id,
-    cs.visit_occurrence_id,
-    cs.condition_source_value
+  cs.person_id,
+  cs.visit_occurrence_id,
+  cs.condition_source_value
 FROM condition_source cs
 JOIN person_map p ON cs.person_id = p.person_id
 JOIN visit_map v ON cs.visit_occurrence_id = v.visit_occurrence_id;
+
