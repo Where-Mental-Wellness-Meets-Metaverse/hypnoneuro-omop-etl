86343db1098cbbbb699bdb664a305f2d2a61a5bc
megmontanez2000 <megmontanez2000@yahoo.com>
2025-05-09 19:08:52 -0400

    Delete observation.sql

--- sql/transforms/observation.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  subject_id AS person_id,
-  chartdate AS observation_date,
-  category AS observation_type,
-  description AS observation_source_value
-FROM `valued-aleph-459311-m0.mimic_demo.noteevents`
-WHERE description IS NOT NULL
-LIMIT 1000;
- 
