b3fea0c3091979be379e155aa4060c79a958d59e
Dr_Meg <megmontanez2000@yahoo.com>
2025-05-09 18:28:16 -0400

    Update drug_exposure.sql
    Create drug_exposure.sql

--- sql/drug_exposure.sql
+++ sql/drug_exposure.sql
@@ -1,11 +1,8 @@
--- drug_exposure.sql
--- Transform MIMIC-III prescriptions into OMOP DRUG_EXPOSURE table
-
 SELECT
-  row_id AS drug_exposure_id,
   subject_id AS person_id,
-  hadm_id AS visit_occurrence_id,
-  drug AS drug_source_value,
   startdate AS drug_exposure_start_date,
-  enddate AS drug_exposure_end_date
-FROM mimic3_demo.prescriptions;
+  drug AS drug_source_value,
+  ndc AS drug_source_concept_id
+FROM `valued-aleph-459311-m0.mimic_demo.prescriptions`
+WHERE drug IS NOT NULL
+LIMIT 1000;
